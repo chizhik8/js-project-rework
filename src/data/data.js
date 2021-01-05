5ee980b44cdf86e48ec11fd5d0245040f3945d60
julia_chizhik <for.yu.ch@gmail.com>
2021-01-05 13:31:24 +0200

    base

---
+++ src/data/data.js
@@ -0,0 +1,23 @@
+
+export const data = {
+  auth: {
+    isAuth: false,
+    accessToken: '',
+  },
+  user: {
+    adv: [],
+    favourites: [],
+    email: '',
+    id: '',
+    registrationDate: '',
+  },
+  calls: {
+    ads: [],
+    categories: [],
+    allCategories: [],
+    russianCategories: [],
+    specificCategory: {},
+    originalCategories: [],
+  },
+  renderedCategories: [],
+};
